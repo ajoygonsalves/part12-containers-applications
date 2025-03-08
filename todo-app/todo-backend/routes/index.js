bc68e43962eaa9ff53d4ea5c32f10208e3dc3de2
ajoyous <ajoy.gonsalves@hotmail.com>
2025-03-08 14:57:25 +0100

    update

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { Todo } = require("../mongo");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+router.get("/todos/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  res.json(todo);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const todo = await Todo.create({
+      text: req.body.text,
+      done: false,
+    });
+    res.status(201).json(todo); // Explicitly send status and end response
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    res.status(500).json({ error: "Failed to create todo" });
+  }
 });
 
 module.exports = router;
